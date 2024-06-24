f9f4e42f30d70b7debaee7729ba8889e01277810
kmriazuddin <mohammadriazuddin3@gmail.com>
2024-06-24 16:29:33 +0600

    Create and initialize Meeting Room Booking System for Co-working spaces

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,26 @@
+import { catchAsync } from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { UserServices } from './user.services';
+
+const createUser = catchAsync(async (req, res) => {
+  const result = await UserServices.createUserIntoDB(req.body);
+
+  const { name, email, phone, role, address, _id } = result;
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'User registered successfully!',
+    data: {
+      _id,
+      name,
+      email,
+      phone,
+      role,
+      address,
+    },
+  });
+});
+
+export const UserControllers = {
+  createUser,
+};
