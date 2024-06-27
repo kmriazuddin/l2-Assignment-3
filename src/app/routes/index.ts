b8db11b4640e3c26ca2a20e60b437679dc80165c
kmriazuddin <mohammadriazuddin3@gmail.com>
2024-06-27 10:11:21 +0600

    created the booking, get all booking, booking update and soft delete of the meeting room booking system

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import { userRoute } from '../modules/user/user.route';
 import { meetingRoomRoute } from '../modules/room/room.route';
 import { slotRoute } from '../modules/slot/slot.route';
-// import { auth } from '../middleware/auth';
-// import { bookingRoute } from '../modules/booking/booking.route';
-// import { BookingControllers } from '../modules/booking/booking.controller';
+import { bookingRoute } from '../modules/booking/booking.route';
+import { auth } from '../middleware/auth';
+import { BookingControllers } from '../modules/booking/booking.controller';
 
 const router = express.Router();
 
@@ -21,13 +21,13 @@ const moduleRoutes = [
     path: '/slots',
     route: slotRoute,
   },
-  // {
-  //   path: '/bookings',
-  //   route: bookingRoute,
-  // },
+  {
+    path: '/bookings',
+    route: bookingRoute,
+  },
 ];
 
-// router.get('/my-bookings', auth('user'), BookingControllers.getUserBooking);
+router.get('/my-bookings', auth('user'), BookingControllers.getUserBooking);
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
