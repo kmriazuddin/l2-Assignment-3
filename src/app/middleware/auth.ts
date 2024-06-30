cce57caf79f4b337e5f3fcb1479e2a4753dd1b50
kmriazuddin <mohammadriazuddin3@gmail.com>
2024-07-01 01:16:26 +0600

    JWT Token Error Problem Solve

--- src/app/middleware/auth.ts
+++ src/app/middleware/auth.ts
@@ -8,7 +8,7 @@ import config from '../config';
 
 export const auth = (...requiredRole: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization?.split(' ')[1];
 
     if (!token) {
       return res.status(httpStatus.UNAUTHORIZED).json({
